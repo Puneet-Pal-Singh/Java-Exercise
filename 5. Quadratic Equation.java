ad58b175251e8cc5514d21dd27d39e4631412021
Puneet Pal Singh <46601256+Puneet-Pal-Singh@users.noreply.github.com>
2019-09-12 22:51:28 +0530

    Create 5. Quadratic Equation.java

---
+++ 5. Quadratic Equation.java
@@ -0,0 +1,2 @@
+// Write a program to print all real solution to the Quadratic Solutions ax^2 + bx+c = 0 . Read in a,b,c and use the quadratic formula.If the disriminant b^2 - 4ac is negative, display message starting that there are no
+real solutions.
