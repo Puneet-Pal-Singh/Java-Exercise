201849ffc025a65d95b5db4fe5c25594912771a1
Puneet Pal Singh <46601256+Puneet-Pal-Singh@users.noreply.github.com>
2019-09-14 21:39:32 +0530

    Update 8. a) String is Palindrome.java

--- 8. a) String is Palindrome.java
+++ 8. a) String is Palindrome.java
@@ -1,1 +1,25 @@
 // Write a program to check whether a given string is palindrome or not. 
+import java.util.Scanner;
+ 
+class Palindrome
+{
+   public static void main(String args[])
+   {
+      String str, r = "";
+      Scanner sc = new Scanner(System.in);
+ 
+      System.out.println("Enter a string:");
+      str = sc.nextLine();
+ 
+      int length = str.length();
+ 
+      for ( int i = length - 1; i >= 0; i-- )
+         r = r + str.charAt(i);
+ 
+      if (str.equals(r))
+         System.out.println(str+" is a palindrome");
+      else
+         System.out.println(str+" is not a palindrome");
+ 
+   }
+}
