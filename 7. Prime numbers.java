8463e5005deb76d64e1e6fe646828fc99ac5ebc9
Puneet Pal Singh <46601256+Puneet-Pal-Singh@users.noreply.github.com>
2019-09-14 22:23:27 +0530

    Update 7. Prime numbers.java

--- 7. Prime numbers.java
+++ 7. Prime numbers.java
@@ -24,3 +24,39 @@ public class Primepr{
 }    
 }   
 
+
+//This program prints the series.
+import java.util.Scanner;
+class PrimeNumberstwo
+{
+   public static void main (String[] args)
+   {		
+      Scanner sc = new Scanner(System.in);
+      int i =0;
+      int num =0;
+      //Empty String
+      String  primeNumbers = "";
+      System.out.println("Enter the value of n:");
+      int n = sc.nextInt();
+      sc.close();
+      for (i = 1; i <= n; i++)  	   
+      { 		 		  
+         int counter=0; 		  
+         for(num =i; num>=1; num--)
+         {
+	    if(i%num==0)
+	    {
+		counter = counter + 1;
+	    }
+	 }
+	 if (counter ==2)
+	 {
+	    //Appended the Prime number to the String
+	    primeNumbers = primeNumbers + i + " ";
+	 }	
+      }	
+      System.out.println("Prime numbers from 1 to n are :");
+      System.out.println(primeNumbers);
+   }
+}
+
