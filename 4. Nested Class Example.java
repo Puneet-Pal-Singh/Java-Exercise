2b0983c8a3ca538dc5d8ca3e6a3191f0bcf00877
Puneet Pal Singh <46601256+Puneet-Pal-Singh@users.noreply.github.com>
2019-10-28 18:37:58 +0530

    Update 4. Nested Class Example.java

--- 4. Nested Class Example.java
+++ 4. Nested Class Example.java
@@ -1,1 +1,20 @@
 // This program demonstrates the use of a nested class 
+class OuterClass 
+{
+    int x = 10;
+    class InnerClass 
+    {
+      int y = 5;
+    }
+}
+  
+public class MyJClass4 
+{
+    public static void main(String[] args)
+    {
+      OuterClass myOuter = new OuterClass();
+      OuterClass.InnerClass myInner = myOuter.new InnerClass();
+      System.out.println(myInner.y + myOuter.x);
+    }
+}
+  
