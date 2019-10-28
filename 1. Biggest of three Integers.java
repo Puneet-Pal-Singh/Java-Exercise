c54e918320807da8549f7f9e948ed1253043060d
Puneet Pal Singh <46601256+Puneet-Pal-Singh@users.noreply.github.com>
2019-10-28 18:36:33 +0530

    Update 1. Biggest of three Integers.java

--- 1. Biggest of three Integers.java
+++ 1. Biggest of three Integers.java
@@ -1,1 +1,30 @@
-# Write a Program to find the biggest of three given Integer numbers or one can ask user to enter the three integer Numbers
+// Write a Program to find the biggest of three given Integer numbers or one can ask user to enter the three integer Numbers
+import java.util.*;
+public class MyJClass1
+{
+    public static void main(String args[])
+    {    
+        int x,y,z;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the First Number:");
+        x = sc.nextInt();      
+        System.out.println("Enter the Second Number:");
+        y = sc.nextInt();
+        System.out.println("Enter the Third Number:");
+        z = sc.nextInt();
+        if(x>y && x>z)
+        {
+            System.out.println("Largest Number is:" + x);
+        }
+        else if(y>z)
+        {
+            System.out.println("Largest Number is:" + y);
+        }
+        else
+        {
+            System.out.println("Largest Number is:" + z);
+        }
+    }
+}
+
+
