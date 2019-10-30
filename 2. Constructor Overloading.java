bb976f9a57aac27402fdfae728c0a0aba831e2b5
Puneet Pal Singh <46601256+Puneet-Pal-Singh@users.noreply.github.com>
2019-10-30 20:55:28 +0530

    Update 2. Constructor Overloading.java

--- 2. Constructor Overloading.java
+++ 2. Constructor Overloading.java
@@ -1,1 +1,43 @@
 //Write a program to define a class , describe its constructor , overlaod the constructor and initiate the object.
+
+import java.util.*;
+
+class Box 
+{ 
+    double width, height, depth; 
+    Box(double w, double h, double d) 
+    { 
+        width = w; 
+        height = h; 
+        depth = d; 
+    } 
+    Box() 
+    { 
+        width = height = depth = 0; 
+    }  
+    Box(double len) 
+    { 
+        width = height = depth = len; 
+    } 
+    double volume() 
+    { 
+        return width * height * depth; 
+    } 
+}  
+public class MyJClass2 
+{ 
+    public static void main(String args[]) 
+    {  
+        Box box1 = new Box(10, 24, 14); 
+        Box box2 = new Box(); 
+        Box box3 = new Box(8); 
+        
+        double vol; 
+        vol = box1.volume(); 
+        System.out.println(" Volume of box1 is " + vol); 
+        vol = box2.volume(); 
+        System.out.println(" Volume of box2 is " + vol); 
+        vol = box3.volume(); 
+        System.out.println(" Volume of box3 is " + vol); 
+    } 
+} 
