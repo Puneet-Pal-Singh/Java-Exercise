ba60dbb4c8f4a13a4850b09d5d2c8344951fb21c
Puneet Pal Singh <46601256+Puneet-Pal-Singh@users.noreply.github.com>
2019-10-31 17:29:59 +0530

    Update 7. Prime numbers.java

--- 7. Prime numbers.java
+++ 7. Prime numbers.java
@@ -1,4 +1,4 @@
-// Write a program to prompt the user for an integer and then print out all the prime numbers up to that integer.
+// This program prints whether a number is prime or not
 import java.util.*;
 public class Primepr{    
  public static void main(String args[])
@@ -58,5 +58,53 @@ class PrimeNumberstwo
       System.out.println("Prime numbers from 1 to n are :");
       System.out.println(primeNumbers);
    }
+}
+
+
+// This program prompts the user for an integer and then print out all the prime numbers up to that integer.
+import java.util.*;
+import java.util.Scanner;
+class MyJClass7
+{
+ static int[] a = new int[100];
+ static int k = 0;
+ public static void PrimeCalc(int n)
+ {
+  int i,j,flag = 0;
+  for(i=2;i<=n;i++)
+  {
+	flag = 0;
+	for(j=2;j<i;j++)
+	{
+		 if(i%j==0)
+	 	{
+	 		flag = 1;
+			break;
+	 	}
+	 	else
+	 	{
+			flag = 0;
+     	}
+	}
+	if(flag == 0)
+	{
+		a[k] = i;
+		k++;
+	}
+		
+  }
+ }
+ 
+ public static void main(String args[])
+ {
+	 int n;
+	 System.out.println("Enter Number upto which Prime numbers are to be found: ");
+	 Scanner s = new Scanner(System.in);
+	 n=s.nextInt();
+	 PrimeCalc(n);
+	 System.out.println("The prime numbers upto " + Integer.toString(n) + " are:");
+	 for(int x=0;x<k;x++)
+		 System.out.println(Integer.toString(a[x]));
+ }
 }
 
