830059a7e7d1f6da956a89de1a10265612608240
Puneet Pal Singh <46601256+Puneet-Pal-Singh@users.noreply.github.com>
2019-10-31 17:24:31 +0530

    Update 5. Quadratic Equation.java

--- 5. Quadratic Equation.java
+++ 5. Quadratic Equation.java
@@ -2,18 +2,19 @@
 // Read in a,b,c and use the quadratic formula.If the disriminant b^2 - 4ac is negative, display message starting that there are no
 // real solutions.
 import java.util.*;
-public class Quadratic
+public class  MyJClass5
 {
     public static void main(String[] args)
     {
         double a, b, c;
         double root1, root2;
         Scanner sc=new Scanner(System.in);
-        System.out.println("enter value of a : ");      
+        System.out.println("Enter the coefficients of the equation:");
+        System.out.println("Enter value of a : ");      
         a=sc.nextDouble();
-        System.out.println("enter value of b : ");
+        System.out.println("Enter value of b : ");
         b=sc.nextDouble();
-        System.out.println("enter value of c : ");
+        System.out.println("Enter value of c : ");
         c=sc.nextDouble();
         double determinant = b * b - 4 * a * c;
     
@@ -21,13 +22,16 @@ public class Quadratic
         {
             root1 = (-b + Math.sqrt(determinant)) / (2 * a);
             root2 = (-b - Math.sqrt(determinant)) / (2 * a);
-            System.out.println("root1 = "+ root1 + "root2 = " + root2);
+            System.out.println("Real and unequal Solutions Exist. They are: ");
+            System.out.println("root1 = "+ root1 + "\nroot2 = " + root2);
         }
       
         else if(determinant == 0)
         {
-            root1 = root2 = -b / (2 * a);
-            System.out.println("root1 = root2 = "+ root1);
+            root1 = -b / (2 * a);
+            root2 = root1;              
+            System.out.println("Real and Equal Solutions Exist. They are: ");
+            System.out.println(root1 +"and"+ root2);
         }
      
         else
