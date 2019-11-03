15663a6615f32b0d529ddea8bab794ca768e9837
Puneet Pal Singh <46601256+Puneet-Pal-Singh@users.noreply.github.com>
2019-11-03 17:04:36 +0530

    Update 10. Line of integers.java

--- 10. Line of integers.java
+++ 10. Line of integers.java
@@ -1,1 +1,30 @@
-//Line of integers
+// Line of integers
+// Program to reads a line of integers and display each integer and the sum of all integers.
+
+import java.util.*;
+
+public class MyJClass10
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\nEnter sequence of integers with space b/w them and press enter : ");
+        String digit = sc.nextLine();
+        StringTokenizer token = new StringTokenizer(digit);
+        int dig=0,sum=0;
+                        
+        System.out.println("\nEntered digits are : ");
+        
+        while(token.hasMoreTokens())
+        {
+            String s = token.nextToken();
+            dig = Integer.parseInt(s);
+            System.out.println(dig+" ");
+            sum = sum+dig;
+        }
+        System.out.println();
+        System.out.println("Sum is : "+sum);
+
+    }
+}
+
