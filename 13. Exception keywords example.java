bcde706382c208ab05a4d05734d1390f36a5fb23
Puneet Pal Singh <46601256+Puneet-Pal-Singh@users.noreply.github.com>
2019-11-10 22:05:08 +0530

    Update 13. Exception keywords example.java

--- 13. Exception keywords example.java
+++ 13. Exception keywords example.java
@@ -1,1 +1,31 @@
 // 5 Exception Keywords
+import java.util.Scanner;
+class MyException extends Exception
+{
+	MyException(int a)
+	{
+		System.out.println("Exception Number: " + a);
+	}
+}
+class MyJClass13
+{
+	static void compute(int a) throws MyException
+	{
+		System.out.println("Called compute(" + a + ")");
+		if(a>10)
+			throw new MyException(a);
+		System.out.println("Normal Exit");
+	}
+	public static void main(String rgs[])
+	{
+		try
+		{
+			compute(1);
+			compute(20);
+		}
+		catch(MyException e)
+		{
+			System.out.println("Caught " + e);
+		}
+	}
+} 
