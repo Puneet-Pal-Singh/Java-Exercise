39536b92c56e93e6dfad7c2046bf8565598700c4
Puneet Pal Singh <46601256+Puneet-Pal-Singh@users.noreply.github.com>
2019-11-18 14:50:35 +0530

    Update 15. Odd-Even Numbers.java

--- Threads/15. Odd-Even Numbers.java
+++ Threads/15. Odd-Even Numbers.java
@@ -1,1 +1,47 @@
+import java.util.*;
+class Counter implements Runnable
+{
+	Thread o;
+	Counter()
+	{
+		o  = new Thread(this, "Odd Thread");
+		o.start();
+	}
+	synchronized public void run()
+	{
+		try
+		{ 
+			for(int i = 2; i>0; i+=2)
+				{
+					System.out.println("Even Thread: " + i);
+					Thread.sleep(500);
+				}
+		}
+
+		catch(InterruptedException e)
+		{
+			System.out.println("Even Interrupted.");
+		}
+	}
+}
+class MyJClass15
+{
+	public static void main(String args[])
+	{
+		new Counter();
+		try
+		{ 
+			for(int i = 1; i>0; i+=2)
+				{
+					System.out.println("Odd Thread: " + i);
+					Thread.sleep(500);
+				}
+		}
+		catch(InterruptedException e)
+		{
+			System.out.println("Odd Interrupted.");
+		}
+	}
+}
+
 
