b7313745bc0334bd7c542b99e48b2b8d116b7a03
Puneet Pal Singh <46601256+Puneet-Pal-Singh@users.noreply.github.com>
2019-12-08 13:45:59 +0530

    Update 26. Desired Style String.java

--- Applet/26. Desired Style String.java
+++ Applet/26. Desired Style String.java
@@ -1,1 +1,19 @@
 
+import java.awt.*;    
+import java.applet.*; 
+/* <applet code = MyELClass25 width = 300 height = 300></applet> */       
+public class MyELClass25 extends Applet    
+ {    
+   Font f1;    
+    
+   public void  init()    
+    {    
+       f1 = new Font("Arial",Font.BOLD,20);    
+	}
+	public void  paint(Graphics g)    
+    {    
+      g.setFont(f1);
+	  g.setColor(Color.GREEN);
+	  g.drawString("Hello",50,50);    
+	}
+ }	
