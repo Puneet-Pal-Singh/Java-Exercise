01b407870e391880c0e91a9d6acfc58b5e417a33
Puneet Pal Singh <46601256+Puneet-Pal-Singh@users.noreply.github.com>
2019-12-08 13:47:52 +0530

    Update 14. Displaying a message.java

--- Applet/14. Displaying a message.java
+++ Applet/14. Displaying a message.java
@@ -1,1 +1,13 @@
 
+import java.awt.*;
+import java.applet.*;
+/* <applet code = MyELClass13 width = 300 height = 300></applet> */
+public class MyELClass13 extends Applet
+{
+	Label l1;
+	public void init()
+	{
+		l1 = new Label("Hi! I'm displaying a message");
+		add(l1);
+	}
+}
