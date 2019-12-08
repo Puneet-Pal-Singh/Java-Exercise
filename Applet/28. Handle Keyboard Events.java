04b1372cb9ef2b1a445db256cfb9f21dd21b8650
Puneet Pal Singh <46601256+Puneet-Pal-Singh@users.noreply.github.com>
2019-12-08 13:46:59 +0530

    Update 28. Handle Keyboard Events.java

--- Applet/28. Handle Keyboard Events.java
+++ Applet/28. Handle Keyboard Events.java
@@ -1,1 +1,52 @@
 
+import java.awt.*;
+import java.awt.event.*;
+import java.applet.*;
+/*<applet code=MyELClass27.java width=300 height=400></applet>*/
+public class MyELClass27 extends Applet
+implements KeyListener
+{
+	int X=20,Y=30;
+	String msg="KeyEvents--->";
+	public void init()
+	{
+		addKeyListener(this);
+		requestFocus();
+		setBackground(Color.green);
+		setForeground(Color.blue);
+	}
+	public void keyPressed(KeyEvent k)
+	{
+		showStatus("KeyDown");
+		int key=k.getKeyCode();
+		switch(key) 
+		{
+			case KeyEvent.VK_UP:
+				showStatus("Move to Up");
+				break;
+			case KeyEvent.VK_DOWN:
+				showStatus("Move to Down");
+				break;
+			case KeyEvent.VK_LEFT:
+				showStatus("Move to Left");
+				break;
+			case KeyEvent.VK_RIGHT:
+				showStatus("Move to Right");
+				break;
+		}
+		repaint();
+	}
+	public void keyReleased(KeyEvent k)
+	{
+		showStatus("Key Up");
+	}
+	public void keyTyped(KeyEvent k)
+	{
+		msg+=k.getKeyChar();
+		repaint();
+	}
+	public void paint(Graphics g)
+	{
+		g.drawString(msg,X,Y);
+	}
+}
